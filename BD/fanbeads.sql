e79a76e7b7ddaf9c4b084389fa4de04fbddc73fc
LalaMarins <almarins34@.com>
2025-10-22 20:56:13 -0300

    banco atualizado

--- BD/fanbeads.sql
+++ BD/fanbeads.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2025 at 11:06 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Tempo de geração: 23-Out-2025 às 01:55
+-- Versão do servidor: 10.4.21-MariaDB
+-- versão do PHP: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,22 +18,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fanbeads`
+-- Banco de dados: `fanbeads`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categoria`
+-- Estrutura da tabela `categoria`
 --
 
 CREATE TABLE `categoria` (
   `id_categoria` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `categoria`
+-- Extraindo dados da tabela `categoria`
 --
 
 INSERT INTO `categoria` (`id_categoria`, `nome`) VALUES
@@ -43,17 +43,17 @@ INSERT INTO `categoria` (`id_categoria`, `nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `opcao_variacao`
+-- Estrutura da tabela `opcao_variacao`
 --
 
 CREATE TABLE `opcao_variacao` (
   `id_opcao` int(11) NOT NULL,
   `variacao_id` int(11) NOT NULL,
   `valor` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `opcao_variacao`
+-- Extraindo dados da tabela `opcao_variacao`
 --
 
 INSERT INTO `opcao_variacao` (`id_opcao`, `variacao_id`, `valor`) VALUES
@@ -67,7 +67,19 @@ INSERT INTO `opcao_variacao` (`id_opcao`, `variacao_id`, `valor`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedidos`
+-- Estrutura da tabela `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(100) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expires_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos`
 --
 
 CREATE TABLE `pedidos` (
@@ -76,19 +88,20 @@ CREATE TABLE `pedidos` (
   `data_pedido` timestamp NOT NULL DEFAULT current_timestamp(),
   `valor_total` decimal(10,2) NOT NULL,
   `status` varchar(50) NOT NULL DEFAULT 'Processando'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pedidos`
+-- Extraindo dados da tabela `pedidos`
 --
 
 INSERT INTO `pedidos` (`id_pedido`, `id_usuario`, `data_pedido`, `valor_total`, `status`) VALUES
-(1, 2, '2025-10-12 21:03:02', 23.80, 'Processando');
+(1, 2, '2025-10-12 21:03:02', '23.80', 'Processando'),
+(2, 2, '2025-10-22 23:46:00', '11.90', 'Processando');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pedido_itens`
+-- Estrutura da tabela `pedido_itens`
 --
 
 CREATE TABLE `pedido_itens` (
@@ -99,19 +112,20 @@ CREATE TABLE `pedido_itens` (
   `preco_unitario` decimal(10,2) NOT NULL,
   `cor` varchar(100) DEFAULT NULL,
   `tamanho` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pedido_itens`
+-- Extraindo dados da tabela `pedido_itens`
 --
 
 INSERT INTO `pedido_itens` (`id_item`, `id_pedido`, `id_produto`, `quantidade`, `preco_unitario`, `cor`, `tamanho`) VALUES
-(1, 1, 6, 2, 11.90, 'Branco', '15');
+(1, 1, 6, 2, '11.90', 'Branco', '15'),
+(2, 2, 5, 1, '11.90', 'Branco', '15');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produto`
+-- Estrutura da tabela `produto`
 --
 
 CREATE TABLE `produto` (
@@ -121,31 +135,31 @@ CREATE TABLE `produto` (
   `preco` decimal(10,2) NOT NULL,
   `imagem` varchar(255) DEFAULT NULL,
   `categoria_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `produto`
+-- Extraindo dados da tabela `produto`
 --
 
 INSERT INTO `produto` (`id_produto`, `nome`, `descricao`, `preco`, `imagem`, `categoria_id`) VALUES
-(2, 'TesteStrap', 'DescriçãoStrap', 15.00, 'img_682683ddba15b.png', 2),
-(5, 'Pulseira de Kpop da Oh My Girl', 'omg', 11.90, 'img_6826875934167.png', 1),
-(6, 'Pulseira da Taylor do Reputation', 'descrição', 11.90, 'img_683a4b304ace5.png', 1),
-(7, 'Pulseira de Epic the Musical - The Wisdom Saga', 'ddesc', 11.90, 'img_683a4b5805e0f.png', 1);
+(2, 'TesteStrap', 'DescriçãoStrap', '15.00', 'img_682683ddba15b.png', 2),
+(5, 'Pulseira de Kpop da Oh My Girl', 'omg', '11.90', 'img_6826875934167.png', 1),
+(6, 'Pulseira da Taylor do Reputation', 'descrição', '11.90', 'img_683a4b304ace5.png', 1),
+(7, 'Pulseira de Epic the Musical - The Wisdom Saga', 'ddesc', '11.90', 'img_683a4b5805e0f.png', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `produto_opcao`
+-- Estrutura da tabela `produto_opcao`
 --
 
 CREATE TABLE `produto_opcao` (
   `produto_id` int(11) NOT NULL,
   `opcao_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `produto_opcao`
+-- Extraindo dados da tabela `produto_opcao`
 --
 
 INSERT INTO `produto_opcao` (`produto_id`, `opcao_id`) VALUES
@@ -161,7 +175,7 @@ INSERT INTO `produto_opcao` (`produto_id`, `opcao_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estrutura da tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -170,10 +184,10 @@ CREATE TABLE `usuario` (
   `email` varchar(100) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `role` enum('admin','user') NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `usuario`
+-- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`id_usuario`, `username`, `email`, `senha`, `role`) VALUES
@@ -183,48 +197,55 @@ INSERT INTO `usuario` (`id_usuario`, `username`, `email`, `senha`, `role`) VALUE
 -- --------------------------------------------------------
 
 --
--- Table structure for table `variacao`
+-- Estrutura da tabela `variacao`
 --
 
 CREATE TABLE `variacao` (
   `id_variacao` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `variacao`
+-- Extraindo dados da tabela `variacao`
 --
 
 INSERT INTO `variacao` (`id_variacao`, `nome`) VALUES
 (1, 'Cor');
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `categoria`
+-- Índices para tabela `categoria`
 --
 ALTER TABLE `categoria`
   ADD PRIMARY KEY (`id_categoria`),
   ADD UNIQUE KEY `nome` (`nome`);
 
 --
--- Indexes for table `opcao_variacao`
+-- Índices para tabela `opcao_variacao`
 --
 ALTER TABLE `opcao_variacao`
   ADD PRIMARY KEY (`id_opcao`),
   ADD KEY `variacao_id` (`variacao_id`);
 
 --
--- Indexes for table `pedidos`
+-- Índices para tabela `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`email`),
+  ADD KEY `token_index` (`token`);
+
+--
+-- Índices para tabela `pedidos`
 --
 ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`id_pedido`),
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
--- Indexes for table `pedido_itens`
+-- Índices para tabela `pedido_itens`
 --
 ALTER TABLE `pedido_itens`
   ADD PRIMARY KEY (`id_item`),
@@ -232,21 +253,21 @@ ALTER TABLE `pedido_itens`
   ADD KEY `id_produto` (`id_produto`);
 
 --
--- Indexes for table `produto`
+-- Índices para tabela `produto`
 --
 ALTER TABLE `produto`
   ADD PRIMARY KEY (`id_produto`),
   ADD KEY `categoria_id` (`categoria_id`);
 
 --
--- Indexes for table `produto_opcao`
+-- Índices para tabela `produto_opcao`
 --
 ALTER TABLE `produto_opcao`
   ADD PRIMARY KEY (`produto_id`,`opcao_id`),
   ADD KEY `opcao_id` (`opcao_id`);
 
 --
--- Indexes for table `usuario`
+-- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`),
@@ -254,88 +275,88 @@ ALTER TABLE `usuario`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indexes for table `variacao`
+-- Índices para tabela `variacao`
 --
 ALTER TABLE `variacao`
   ADD PRIMARY KEY (`id_variacao`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `categoria`
+-- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
   MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `opcao_variacao`
+-- AUTO_INCREMENT de tabela `opcao_variacao`
 --
 ALTER TABLE `opcao_variacao`
   MODIFY `id_opcao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `pedidos`
+-- AUTO_INCREMENT de tabela `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `pedido_itens`
+-- AUTO_INCREMENT de tabela `pedido_itens`
 --
 ALTER TABLE `pedido_itens`
-  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `produto`
+-- AUTO_INCREMENT de tabela `produto`
 --
 ALTER TABLE `produto`
   MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- AUTO_INCREMENT for table `usuario`
+-- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `variacao`
+-- AUTO_INCREMENT de tabela `variacao`
 --
 ALTER TABLE `variacao`
   MODIFY `id_variacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Restrições para despejos de tabelas
 --
 
 --
--- Constraints for table `opcao_variacao`
+-- Limitadores para a tabela `opcao_variacao`
 --
 ALTER TABLE `opcao_variacao`
   ADD CONSTRAINT `opcao_variacao_ibfk_1` FOREIGN KEY (`variacao_id`) REFERENCES `variacao` (`id_variacao`);
 
 --
--- Constraints for table `pedidos`
+-- Limitadores para a tabela `pedidos`
 --
 ALTER TABLE `pedidos`
   ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 
 --
--- Constraints for table `pedido_itens`
+-- Limitadores para a tabela `pedido_itens`
 --
 ALTER TABLE `pedido_itens`
   ADD CONSTRAINT `pedido_itens_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`) ON DELETE CASCADE,
   ADD CONSTRAINT `pedido_itens_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`);
 
 --
--- Constraints for table `produto`
+-- Limitadores para a tabela `produto`
 --
 ALTER TABLE `produto`
   ADD CONSTRAINT `produto_ibfk_1` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id_categoria`);
 
 --
--- Constraints for table `produto_opcao`
+-- Limitadores para a tabela `produto_opcao`
 --
 ALTER TABLE `produto_opcao`
   ADD CONSTRAINT `produto_opcao_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id_produto`),
